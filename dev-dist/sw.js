b26d12121478e1b7e22b7af21fa381e6c4a42cc7
Your Name <you@example.com>
2026-08-26 12:40:15 +0700

    chore: update index.html revision in service worker manifest

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-b1bafff1'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.3pqqgr7j6kc"
+    "revision": "0.l6auvub9b4g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
