ccd47af090046e0e5a395038f9360805c2e046bb
Your Name <you@example.com>
2026-08-26 16:11:59 +0700

    chore: remove entire api-service directory and its contents

--- api-service/src/common/utils/order-id.util.spec.ts
+++
@@ -1,8 +0,0 @@
-import { generateOrderId } from './order-id.util';
-
-describe('generateOrderId', () => {
-  it('should generate order ID matching EPS-YYYYMMDD-XXXX format', () => {
-    const orderId = generateOrderId();
-    expect(orderId).toMatch(/^EPS-\d{8}-\d{4}$/);
-  });
-});
